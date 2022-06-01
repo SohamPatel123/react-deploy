6c671bfa29b8d7bb65eab5e00b214e0a810a2b4b
Soham Patel <soham.patel@sprinklr.com>
2022-06-01 09:05:32 +0530

    Updates

---
+++ precache-manifest.2ab801af1203a2331364658c5fa9ff91.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "2981d5993fe65b2f52fb",
+    "url": "/react-deploy/static/css/main.ae84863b.chunk.css"
+  },
+  {
+    "revision": "2981d5993fe65b2f52fb",
+    "url": "/react-deploy/static/js/main.2981d599.chunk.js"
+  },
+  {
+    "revision": "1497e999a888b9fa61ca",
+    "url": "/react-deploy/static/js/1.1497e999.chunk.js"
+  },
+  {
+    "revision": "e5dcd1f2857660193783",
+    "url": "/react-deploy/static/js/2.e5dcd1f2.chunk.js"
+  },
+  {
+    "revision": "ccce8bf1dcdde39e9967",
+    "url": "/react-deploy/static/js/runtime~main.ccce8bf1.js"
+  },
+  {
+    "revision": "3003fd83d737622d1cc392c89d914a18",
+    "url": "/react-deploy/index.html"
+  }
+];
